3b5c2533b91c96b47c1d306b8fb09a8de8d8af3f
ali tavakoli <alitavakoli2018@gmail.com>
2019-06-04 19:28:33 +0430

    create edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,20 @@ class ArticlesController < ApplicationController
         @article=Article.new
     end
 
+    def edit
+        @article=Article.find(params[:id])
+    end
+
+    def update
+        @article=Article.new(article_params)
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end    
+    end
+
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -12,10 +26,11 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    
 
-        def show
-            @article=Article.find(params[:id])
-        end
+    def show
+        @article=Article.find(params[:id])
+    end
         
     end
 
@@ -23,4 +38,4 @@ class ArticlesController < ApplicationController
         def article_params
             params.require(:article).permit(:title,:description)
         end
-end 
+    end
